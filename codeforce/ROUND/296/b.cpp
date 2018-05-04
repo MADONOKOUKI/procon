9b27b19756e6ac0b08ce6445ca65cbab767e152a
MADONOKOUKI <madonomadonorunning@gmail.com>
2018-05-04 20:21:49 +0900

    auto commit

--- codeforce/ROUND/296/b.cpp
+++ codeforce/ROUND/296/b.cpp
@@ -1,36 +1,54 @@
-#include <bits/stdc++.h>
-
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
 using namespace std;
-
-typedef long long ll;
-
-const int INF = 1000000;
-
-bool myoperator(const pair<int, int> left, const pair<int, int> right){
-  if(left.first != right.first){
-    return (left.first < right.first);
-  // }else{
-  //   return (left.second >= right.second);
-  // }
-}
-
-int main(){
-  cin.tie(0);
-  ios_base::sync_with_stdio(false);
-  int N; cin >> N;
-  vector<pair<int, int> > pr;
-  for(int i=0;i<N;i++){
-    int x, w; cin >> x >> w;
-    pr.push_back(make_pair(x, w));
+#define SIZE 262144
+class segtree
+{
+public:
+  int seg[SIZE * 2];
+  void update(int a, int b)
+  {
+    a += SIZE;
+    seg[a] = max(seg[a], b);
+    for (;;)
+    {
+      a /= 2;
+      if (a == 0)break;
+      seg[a] = max(seg[a * 2], seg[a * 2 + 1]);
+    }
   }
-  sort(pr.begin(), pr.end(), myoperator);
-  for(int i=0;i<N;i++){
-    cout<<pr[i].first<<" "<<pr[i].second<<endl;
+  int get(int beg, int end, int node, int lb, int ub)
+  {
+    if (ub < beg || end < lb)return 0;
+    if (beg <= lb&&ub <= end)return seg[node];
+    return max(get(beg, end, node * 2, lb, (lb + ub) / 2), get(beg, end, node * 2 + 1, (lb + ub) / 2 + 1, ub));
   }
-  int dp[N];
-  fill(dp, dp+N, INF);
-  for(int i=0;i<N;i++){
-    *lower_bound(dp, dp+N, pr[i].first + pr[i].second) = pr[i].first + pr[i].second;
+};
+segtree tree;
+typedef pair<int, int>pii;
+int main()
+{
+  int num;
+  scanf("%d", &num);
+  vector<pii>vec;
+  vector<int>zv;
+  for (int i = 0; i < num; i++)
+  {
+    int za, zb;
+    scanf("%d%d", &za, &zb);
+    vec.push_back(make_pair(za, zb));
+    zv.push_back((za + zb));
   }
-  cout << lower_bound(dp, dp+N, INF) - dp << endl;
+  sort(vec.begin(), vec.end());
+  sort(zv.begin(), zv.end());
+  for (int i = 0; i < num; i++)
+  {
+    int upp = upper_bound(zv.begin(), zv.end(), vec[i].first - vec[i].second) - zv.begin() - 1;
+    int g = 0;
+    if (upp >= 0)g = tree.get(0, upp, 1, 0, SIZE - 1);
+    int low = lower_bound(zv.begin(), zv.end(), vec[i].first + vec[i].second) - zv.begin();
+    tree.update(low, g + 1);
+  }
+  printf("%d\n", tree.get(0, SIZE - 1, 1, 0, SIZE - 1));
 }
